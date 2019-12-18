e1a2c73d50c07d88234dd126dff65674ae0b92fc
Rosen Kanev <rkunev@gmail.com>
2019-12-18 14:33:54 +0200

    style: Add Prettier

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import ColorPicker from './ColorPicker';
 
 class App extends React.Component {
-  state = {
-    hue: 90,
-    saturation: 100,
-    luminosity: 50,
-    alpha: 1,
-  };
+    state = {
+        hue: 90,
+        saturation: 100,
+        luminosity: 50,
+        alpha: 1,
+    };
 
-  onInput = (hue) => {
-    this.setState({ hue });
-  };
+    onInput = hue => {
+        this.setState({ hue });
+    };
 
-  render() {
-    return (
-        <div className="App">
-            <ColorPicker {...this.state} onInput={this.onInput} />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <ColorPicker {...this.state} onInput={this.onInput} />
+            </div>
+        );
+    }
 }
 
 export default App;
